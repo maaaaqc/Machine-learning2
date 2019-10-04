853d31b95328e5e65f5390a4b6b3d2f1dd8073e0
Qingchuan Ma <qingchuan.ma@mail.mcgill.ca>
2019-10-03 22:29:29 -0400

    Added url and underscore cleaning

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,6 +5,7 @@ import numpy as np
 import spacy
 import json
 import csv
+import re
 
 
 nlp = spacy.load("en", disable=['parser', 'ner', 'tagger'])
@@ -31,9 +32,13 @@ def process():
     train_set = read_csv(FILEPATH)
     train_x = train_set[:, 0]
     train_y = train_set[:, 1]
-    train_x = lemmatize_all(train_x)
+    for i in range(train_x.shape[0]):
+        train_x[i] = clean_url(train_x[i])
+        train_x[i] = clean_underscore(train_x[i])
+        train_x[i] = lemmatize_all(train_x[i])
     train_x = count_vectorize_all(train_x)
     train_y = categorize(train_y)
+    return [train_x, train_y]
 
 
 def categorize(train_y):
@@ -49,6 +54,7 @@ def categorize(train_y):
 def count_vectorize_all(train_x):
     vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 1), stop_words='english')
     output = vectorizer.fit_transform(train_x).toarray()
+    np.savetxt("feature.txt", vectorizer.get_feature_names(), fmt="%s")
     return output
 
 
@@ -58,9 +64,18 @@ def tfidf_vectorize_all(train_x):
     return output
 
 
+def clean_url(data):
+    data = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'url', data)
+    return data
+
+
+def clean_underscore(data):
+    data = np.str_(" ".join(str(data).split("_")))
+    return data
+
+
 def lemmatize_all(data):
-    for i in range(data.shape[0]):
-        data[i] = np.str_(" ".join([token.lemma_ for token in nlp(str(data[i]))]))
+    data = np.str_(" ".join([token.lemma_ for token in nlp(str(data))]))
     return data
 
 
