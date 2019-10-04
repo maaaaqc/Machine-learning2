0a93c482daff1113627e9c782e8c642dcf02edad
zhuangzhuang <chichengzheng1314@gmail.com>
2019-10-03 22:35:22 -0400

    completed bayes model

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,5 @@
 import Preprocessing
-
+import numpy as np
 
 class NaiveBayes:
     def __init__(self, x, y):
@@ -11,8 +11,31 @@ class NaiveBayes:
         if self.y == i:
             count += 1
 
-    def count_occurrance(self):
+
+    # theta j,k
+    def count_occurrance(self, j, k):
+        # (# of examples where xj = 1 and y = k)/(# of examples where y = k)
+        sum1 = 1
+        sum2 = 2
+        for i in range(self.x.shape[0]):
+            if self.y[i] == k:
+                sum2 = sum2 + 1
+                if self.x[i][j] == 1:
+                    sum1 = sum1 + 1
+        return sum1 / sum2
         pass
 
-    def predict(self):
-        pass
+    def predict(self, x_test):
+        max_probability = 0
+        max_probability_index = -1
+        for i in range(self.y):
+            # k = y[i]
+            t1 = np.math.factorial(np.sum(x_test))
+            t2 = np.prod(x_test)
+            total = 1
+            for j in range(x_test.shape()[1]):
+                total *= np.math.pow(self.count_occurrance(j, self.y[i]), x_test[j])
+            if (t1 / t2 * total > max_probability):
+                max_probability = t1 / t2 * total
+                max_probability_index = self.y[i]
+        return max_probability_index
